479164c4166553999136a9ea649af1deef5d2892
Benjamin Kato <katobeny@yahoo.com>
2022-06-12 22:28:16 +0300

    feat: text component font change

--- app/src/main/java/com/example/touchreflex/draw/text/InfoText.kt
+++ app/src/main/java/com/example/touchreflex/draw/text/InfoText.kt
@@ -1,16 +1,18 @@
 package com.example.touchreflex.draw.text
 
 import android.graphics.Paint
+import android.text.TextPaint
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
 import com.example.touchreflex.R
 import com.example.touchreflex.draw.CustomDrawable
+import com.example.touchreflex.utils.FontCache
 
 abstract class InfoText(
     private val parentView: View
 ) : CustomDrawable {
 
-    protected val paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    protected val paint: TextPaint = TextPaint(Paint.ANTI_ALIAS_FLAG)
     protected val textSize: Float = 100f
 
     init {
@@ -24,6 +26,7 @@ abstract class InfoText(
         paint.style = Paint.Style.FILL
         paint.textAlign = Paint.Align.CENTER
         paint.textSize = textSize
+        paint.typeface = FontCache.get(R.font.kdamthmorproregular, parentView.context)
         paint.setShadowLayer(5.5f, 6.0f, 6.0f, parentView.resources.getColor(R.color.grey, null))
     }
 
